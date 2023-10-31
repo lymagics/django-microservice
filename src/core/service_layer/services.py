658234eda07916db3d029ce2517f124f6131b07d
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-10-31 18:26:47 +0200

    add post get service.

--- src/core/service_layer/services.py
+++ src/core/service_layer/services.py
@@ -1,4 +1,7 @@
+from uuid import UUID
+
 from core.domain import model
+from core.errors import PostNotFound
 from core.service_layer import unit_of_work
 
 
@@ -9,4 +12,16 @@ def post_create(
     with uow:
         post = uow.posts.add(model.Post(text, auhtor))
         uow.commit()
+    return post
+
+
+def post_get(
+    post_id: UUID, 
+    uow: unit_of_work.AbstractUnitOfWork,
+) -> model.Post:
+    with uow:
+        post = uow.posts.get(post_id)
+    if post is None:
+        error = 'Post not found.'
+        raise PostNotFound(error)
     return post
