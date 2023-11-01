20d8a0b428d83b6feb9c43923b4edd67e20a174c
Viacheslav Lymanskyi <viacheslav.lymanskyi@gmail.com>
2023-11-01 11:21:11 +0200

    add endpoint to create post.

--- src/api/views.py
+++ src/api/views.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import yaml
 from ninja import NinjaAPI
 
+from api import schemas
+from core.service_layer import services, unit_of_work
+
 api = NinjaAPI()
 
 oas_file = Path(__file__).parent / '../../oas.yaml'
@@ -10,6 +13,9 @@ oas_docs = yaml.safe_load(oas_file.read_text())
 api.get_openapi_schema = lambda: oas_docs
 
 
-@api.get('/ping')
-def ping(request):
-    return {'msg': 'pong'}
+@api.post('/posts', response=schemas.PostOut)
+def post_create(request, data: schemas.PostIn):
+    return services.post_create(
+        data.text, data.author,
+        unit_of_work.DjangoUnitOfWork(),
+    )
